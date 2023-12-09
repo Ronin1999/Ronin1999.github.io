3cac4d6d5fa5fda49beb676250ca43d645206211
Ronin1999 <mamadjavad78jvd@gmail.com>
2023-12-09 23:33:24 +0330

    updated some files

--- loopStudios/main.js
+++ loopStudios/main.js
@@ -3,13 +3,16 @@ const openBtn = document.querySelector('.menu-open-btn');
 const closeBtn = document.querySelector('.menu-close-btn');
 
 
-openBtn.addEventListener('click', (e)=> {
+openBtn.addEventListener('click', openMenu);
+closeBtn.addEventListener('click', closeMenu);
+
+function openMenu() {
     mobileMenu.setAttribute('aria-open',"true"),
     openBtn.setAttribute('aria-expanded',"true"),
     document.body.classList.add('no-sc');
-});
-closeBtn.addEventListener('click', (e)=> {
+}
+function closeMenu() {
     mobileMenu.setAttribute('aria-open','false'),
     openBtn.setAttribute('aria-expanded',"false"),
     document.body.classList.remove('no-sc');
-});
+}
